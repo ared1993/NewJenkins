3ab8137cb6105f11d14c590e01f21ac7fb751534
ared1993 <eraredinesh@gmail.com>
2019-09-15 16:28:57 +0530

    Change fot git test

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -19,14 +19,13 @@ public class HelloTest {
 	WebElement text;
 	@BeforeTest
 	public void beforeTest() {
-		/*	System.setProperty("webdriver.gecko.driver", "resources\\geckodriver32.exe");
-		driver = new FirefoxDriver();*/
 		System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("http://localhost:8080/HelloWorld.war/");
 		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
 	}
 
+	// Add comment for git test 1
 	@Test
 	public void f() {
 
